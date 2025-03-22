86862e79f56503eeb0356c43634749a35f350c39
kradunches <sasha10131310@gmail.com>
2025-03-22 10:28:11 +0300

    deleted vs template code. added tests

---
+++ Tests/GameTests.cs
@@ -0,0 +1,56 @@
+﻿using KeyStore.Controllers;
+using KeyStore.Models;
+using KeyStore.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Tests
+{
+    public class GameTests
+    {
+        [Fact]
+        public void Get_ReturnsActualGames()
+        {
+            // Arrange
+            var mockLogger = new Mock<ILogger<GameController>>();
+            var mockRepo = new Mock<IGameRepository>();
+            var expectedGames = new List<Game>
+            {
+                new Game { id = 1, name = "Starcraft Brood War", price = 500, genre = "RTS" },
+                new Game { id = 2, name = "Diablo II", price = 1000, genre = "RPG" },
+                new Game { id = 3, name = "Balatro", price = 550, genre = "Rouge Like" },
+                new Game { id = 4, name = "Ultrakill", price = 450, genre = "Shooter" },
+                new Game { id = 5, name = "Noita", price = 700, genre = "Rouge Like" }
+            };
+            mockRepo.Setup(repo => repo.GetGames).Returns(expectedGames);
+
+            var controller = new GameController(mockLogger.Object, mockRepo.Object);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedGames = Assert.IsAssignableFrom<IEnumerable<Game>>(okResult.Value);
+            Assert.Equal(expectedGames.Count, returnedGames.Count());
+            Assert.Equal(expectedGames, returnedGames);
+        }
+        [Fact]
+        public void Get_ReturnsBadRequestResult_WhenModelStateIsInvalid()
+        {
+            // Arrange
+            var mockLogger = new Mock<ILogger<GameController>>();
+            var mockRepo = new Mock<IGameRepository>();
+            var controller = new GameController(mockLogger.Object, mockRepo.Object);
+            controller.ModelState.AddModelError("error", "some error");
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+    }
+}
